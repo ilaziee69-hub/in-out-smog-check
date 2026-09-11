664433f4c01dc88bc892e557cd22bfcc242a44b8
ilaziee69-hub <ilaziee69@gmail.com>
2026-09-10 22:37:23 -0700

    Assemble approved storefront image

---
+++ frontend/src/approved-storefront.js
@@ -0,0 +1,6 @@
+import partA from "@/approved-storefront-a";
+import partB from "@/approved-storefront-b";
+
+const approvedStorefront = `data:image/jpeg;base64,${partA}${partB}`;
+
+export default approvedStorefront;
