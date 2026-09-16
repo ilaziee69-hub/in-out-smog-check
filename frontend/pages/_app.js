3c56f058da0b0cee778dcc910907438779ec1649
ilaziee69-hub <ilaziee69@gmail.com>
2026-09-16 11:58:36 -0700

    Load homepage contrast fixes

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,6 +4,7 @@ import "@/final-home.css";
 import "@/mockup-theme.css";
 import "@/mobile-hero.css";
 import "@/placentia.css";
+import "@/fixes.css";
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
