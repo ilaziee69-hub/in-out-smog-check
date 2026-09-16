dc2089b4ad90573acf65a6a48b3cb17470d8b968
ilaziee69-hub <ilaziee69@gmail.com>
2026-09-16 11:59:18 -0700

    Load contrast fixes in production

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,6 +5,7 @@ import "@/mockup-theme.css";
 import "@/mobile-hero.css";
 import "@/consistency.css";
 import "@/placentia.css";
+import "@/fixes.css";
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
