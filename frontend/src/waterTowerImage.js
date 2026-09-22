1e98446e89e0d1d1da3cced8eb7cc1baefdc9ad9
ilaziee69-hub <ilaziee69@gmail.com>
2026-09-21 21:48:54 -0700

    Reduce external landmark image payloads

--- frontend/src/waterTowerImage.js
+++ frontend/src/waterTowerImage.js
@@ -1,1 +1,1 @@
-export default "https://i0.wp.com/voiceofoc.org/wp-content/uploads/2023/06/IMG_2549-2.jpg?fit=1500%2C844&quality=89&ssl=1";
+export default "https://i0.wp.com/voiceofoc.org/wp-content/uploads/2023/06/IMG_2549-2.jpg?resize=800%2C450&quality=55&ssl=1";
