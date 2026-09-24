ef36fbf1bc06d8af0edc93a1d2dc6b6c3fadea35
ilaziee69-hub <ilaziee69@gmail.com>
2026-09-23 17:06:49 -0700

    Serve homepage without hydration JavaScript

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,4 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
-export default function Document() {
-  return <Html lang="en"><Head /><body><Main /><NextScript /></body></Html>;
+
+export default function Document(props) {
+  const isHome = props.__NEXT_DATA__?.page === "/";
+
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        {!isHome && <NextScript />}
+      </body>
+    </Html>
+  );
 }
